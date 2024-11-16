61dc68d6b10b9209aefd2d8ddf9b296032473673
phhuy7 <phhuy7@gmail.com>
2024-11-17 02:17:56 +0700

    first commit

--- BE/src/validators/registerValidation.js
+++
@@ -1,9 +0,0 @@
-const { body } = require('express-validator');
-
-const registerValidation = [
-  body('username').not().isEmpty().withMessage('Username is required'),
-  body('password').isLength({ min: 6 }).withMessage('Password should be at least 6 characters long'),
-  body('email').isEmail().withMessage('Please enter a valid email'),
-];
-
-module.exports = registerValidation;
